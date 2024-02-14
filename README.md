a11290fcd9d2166a4b38a68aef8aaf04180beb68
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-02-14 11:36:47 +0100

    Base project completed

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Journal
 =======
-This here's a test project based on the following link: 'https://learn.microsoft.com/sv-se/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&WT.mc_id=dotnet-35129-website&tabs=visual-studio'. I'll be documenting my process to creating the ASP.NET API described in the tutorial.
+This here's a small project based on the following link: 'https://learn.microsoft.com/sv-se/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&WT.mc_id=dotnet-35129-website&tabs=visual-studio'. I'll be documenting my process to creating the ASP.NET API described in the tutorial.
 
 2024-02-12
 -----------
@@ -15,3 +15,7 @@ I'm following the tutorial, as I mentioned above, but I'm also changing a few th
 Also I did not expect the scaffold item to generate a full-on controller class for the API. So that caught me off guard.
 
 I've now tested all the HTTP methods, so I've uset Get, Get({"id"}), Post, Put, and Delete. Nothing new; I've done this before. I'm actually pretty sure I've completed this exact tutorial before as well, but not completely certain. Good practice to redo it though, I think. As stated previously, I did completed these tests with Postman. Next up is to prevent over-posting.
+
+2024-02-14
+-----------
+The rest of the project went well, I ran into no issues. Creating the DTO model and implementing it was new territory for me, so it was a little less obvious to me compared to the other things in the tutorial. So, the project as a whole meant setting up a small API using an in-memory database that handles various HTTP methods to retrieve and send data to the database. The DTO model was for securing a 'secret' string that only authenticated users are to be able to access, unless I am mistaken.
--- ToDoApi/Controllers/TodoItemsController.cs
+++ ToDoApi/Controllers/TodoItemsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Data;
 using TodoApi.Models;
@@ -23,14 +18,16 @@ namespace ToDoApi.Controllers
 
         // GET: api/TodoItems
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
         {
-            return await _context.TodoItems.ToListAsync();
+            return await _context.TodoItems
+                .Select(x => ItemToDTO(x))
+                .ToListAsync();
         }
 
         // GET: api/TodoItems/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
         {
             var todoItem = await _context.TodoItems.FindAsync(id);
 
@@ -39,35 +36,35 @@ namespace ToDoApi.Controllers
                 return NotFound();
             }
 
-            return todoItem;
+            return ItemToDTO(todoItem);
         }
 
         // PUT: api/TodoItems/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        public async Task<IActionResult> PutTodoItem(long id, TodoItemDTO todoDTO)
         {
-            if (id != todoItem.Id)
+            if (id != todoDTO.Id)
             {
                 return BadRequest();
             }
 
-            _context.Entry(todoItem).State = EntityState.Modified;
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.Name = todoDTO.Name;
+            todoItem.IsComplete = todoDTO.IsComplete;
 
             try
             {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
             {
-                if (!TodoItemExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return NotFound();
             }
 
             return NoContent();
@@ -76,13 +73,24 @@ namespace ToDoApi.Controllers
         // POST: api/TodoItems
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        public async Task<ActionResult<TodoItemDTO>> PostTodoItem(TodoItemDTO todoDTO)
         {
+            var todoItem = new TodoItem
+            {
+                Name = todoDTO.Name,
+                IsComplete = todoDTO.IsComplete
+            };
+
             _context.TodoItems.Add(todoItem);
             await _context.SaveChangesAsync();
 
+            return CreatedAtAction(
+                nameof(GetTodoItem),
+                new { id = todoItem.Id },
+                ItemToDTO(todoItem));
+
             //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+            //return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
         }
 
         // DELETE: api/TodoItems/5
@@ -105,5 +113,13 @@ namespace ToDoApi.Controllers
         {
             return _context.TodoItems.Any(e => e.Id == id);
         }
+
+        private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
+            new TodoItemDTO
+            {
+                Id = todoItem.Id,
+                Name = todoItem.Name,
+                IsComplete = todoItem.IsComplete
+            };
     }
 }
